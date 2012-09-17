221094407113af26fe9154e44b828aa626e86983
Dave Brooks <dave@bcs.co.nz>
2012-09-17 20:51:31 +1200

    Track widgets with term expressions.

--- query/runscroll.py
+++ query/runscroll.py
@@ -8,6 +8,9 @@ from termgrid import TermGrid
 from scroll import Ui_Form
 
 
+MAXROWS = 3   #: Maximum number of term expressions
+
+
 class QueryForm(QtGui.QWidget):
 #==============================
 
@@ -21,10 +24,10 @@ class QueryForm(QtGui.QWidget):
       )
     self.config = config
     self.ui.expression.set_configuration(config)
-    self._last_row = 0
+    self._rows = [ [ self.ui.operation, self.ui.expression ] ]
+    self._lastrow = 0
     self.ui.operation.hide()
     self.add_operation(1)
-
 
   def add_operation(self, row):
   #----------------------------
@@ -32,20 +35,22 @@ class QueryForm(QtGui.QWidget):
     op.row = row
     self.ui.gridLayout.addWidget(op, row, 1, QtCore.Qt.AlignTop)
     op.currentIndexChanged.connect(self.on_operation_changed)
-
+    self._rows.append([ op, None ])
 
   def on_operation_changed(self, index):
   #-------------------------------------
     op = QtCore.QObject.sender(self)
     if (index > 0
-     and op.row == (self._last_row + 1)
+     and op.row == (self._lastrow + 1)
      and str(op.itemText(0)).startswith('More')):
       item = self.ui.expression.clone('expression%d' % op.row)
       item.set_configuration(self.config)
       self.ui.gridLayout.addWidget(item, op.row, 2)
+      self._rows[op.row][1] = item
       op.removeItem(0)
-      if op.row < 2: self.add_operation(op.row+1)
-      self._last_row += 1
+      if op.row < (MAXROWS - 1): self.add_operation(op.row+1)
+      self._lastrow += 1
+
 
 
 
