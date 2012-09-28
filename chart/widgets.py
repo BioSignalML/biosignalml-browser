11970000c5a74cf4556428bddb0510f5a714a8d4
Dave Brooks <dave@bcs.co.nz>
2012-09-28 14:03:43 +1200

    Add documentation.

--- chart/widgets.py
+++ chart/widgets.py
@@ -1,8 +1,18 @@
+"""
+Sub-class QT classes for widget promotion.
+
+
+"""
+
 from PyQt4 import QtCore, QtGui
 
 
 class ChartFrame(QtGui.QFrame):
 #==============================
+
+  """
+  Emit a signal when the size of the chart's frame changes.
+  """
 
   frameResize = QtCore.pyqtSignal(QtCore.QRect)
 
