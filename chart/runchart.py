437894803f2f460896460b945d44c364350f109a
Dave Brooks <dave@bcs.co.nz>
2012-09-28 18:10:36 +1200

    Add controller interface.

--- chart/runchart.py
+++ chart/runchart.py
@@ -4,6 +4,7 @@ import logging
 from PyQt4 import QtCore, QtGui
 
 from ui.chart import Ui_Form
+from ui.controller import Ui_Controller
 
 import biosignalml.formats.hdf5 as hdf5
 import biosignalml.formats.edf  as edf
@@ -75,6 +76,104 @@ class ChartForm(QtGui.QWidget):
     self.ui.chart.contextMenu(pos)
 
 
+
+class SignalInfo(QtCore.QAbstractTableModel):
+#============================================
+
+  HEADER = [ '', 'Label', 'Uri' ]
+
+  def __init__(self, *args, **kwds):
+  #---------------------------------
+    QtCore.QAbstractTableModel.__init__(self, *args, **kwds)
+    self._rows = [ [True,  'Signal 1', 'http://example.org/signal/1'],
+                   [True,  'Signal 2', 'http://example.org/signal/2'],
+                   [False, 'Signal 3', 'http://example.org/signal/3'],
+                   [True,  'Signal 4', 'http://example.org/signal/4'],
+                 ]
+
+  def rowCount(self, parent=None):
+  #-------------------------------
+    return len(self._rows)
+
+  def columnCount(self, parent=None):
+  #----------------------------------
+    return len(self.HEADER)
+
+  def headerData(self, section, orientation, role):
+  #------------------------------------------------
+    if role == QtCore.Qt.DisplayRole:
+      if orientation == QtCore.Qt.Horizontal:
+        return self.HEADER[section]
+
+  def data(self, index, role):
+  #---------------------------
+    if   role == QtCore.Qt.DisplayRole:
+      if index.column() != 0:
+        return self._rows[index.row()][index.column()]
+      else:
+        return QtCore.QVariant()
+    elif role == QtCore.Qt.CheckStateRole:
+      if index.column() == 0:
+        return QtCore.Qt.Checked if self._rows[index.row()][0] else QtCore.Qt.Unchecked
+
+  def setData(self, index, value, role):
+  #-------------------------------------
+    if role == QtCore.Qt.CheckStateRole and index.column() == 0:
+      self._rows[index.row()][0] = (value == QtCore.Qt.Checked)
+      self.dataChanged.emit(index, index)
+      return True
+    return False
+
+  def moveRow(self, sourceindex, sourcerow, destindex, destrow):
+  #-------------------------------------------------------------
+    data = self._rows[sourcerow]
+    if sourcerow > destrow:        # Moving up
+      self._rows[destrow+1:sourcerow+1] = self._rows[destrow:sourcerow]
+      self._rows[destrow] = data
+    elif (sourcerow+1) < destrow:  # Moving down
+      self._rows[sourcerow:destrow-1] = self._rows[sourcerow+1:destrow]
+      self._rows[destrow-1] = data
+
+  def flags(self, index):
+  #-----------------------
+    if index.column() == 0:
+      return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsSelectable
+    else:
+      return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+
+
+
+class Controller(QtGui.QWidget):
+#===============================
+
+  def __init__(self, recording, start, duration, order=None, parent=None):
+  #-----------------------------------------------------------------------
+    QtGui.QWidget.__init__(self, parent)
+    self.controller = Ui_Controller()
+    self.controller.setupUi(self)
+    self.controller.signals.setModel(SignalInfo(recording, order))
+
+
+    self.viewer = ChartForm(start, duration, self)
+
+    # Connect with signals/slots....
+
+    for n, s in enumerate(recording.signals()):
+      if s.rate:                  ###### Need attribute for Signal
+        label = "V5" if n == 0 else "V2" if n == 1 else "S%d" % n  #########
+        units = "mV"
+        self.viewer.addSignalPlot(n, label, units)
+      else:                       ###### or Annotation...
+        label = 'atr'   ##########
+        self.viewer.addEventPlot(n, label, wfdbAnnotation)
+
+      for d in s.read(recording.interval(start, duration)):
+        self.viewer.appendData(n, d)
+
+   self.viewer.orderPlots(order if order else [])  # via controller??
+
+
+
 if __name__ == "__main__":
 #=========================
 
@@ -94,9 +193,14 @@ if __name__ == "__main__":
 
   app = QtGui.QApplication(sys.argv)
 
-  start = 90
-  duration = 20
-  viewer1 = ChartForm(start, duration)
+
+  record1 = 'mitdb/102'
+  rec1 = hdf5.HDF5Recording.open('/physiobank/database/%s.h5' % record1)
+  ctl1 = Controller(rec1, 90, 20, [0, 2, 1])
+  ctl1.show()
+  ctl1.raise_()
+
+  """
   record1 = 'mitdb/102'
   rec1 = hdf5.HDF5Recording.open('/physiobank/database/%s.h5' % record1)
   for n, s in enumerate(rec1.signals()):
@@ -115,8 +219,7 @@ if __name__ == "__main__":
 
   #viewer1.save_chart_as_png('test.png')   ## Needs to be via 'Save' button/menu and file dialog...
 
-  viewer1.raise_()
-
+  """
 
 
   """
---
+++ chart/signaltable.py
@@ -0,0 +1,86 @@
+from PyQt4 import QtCore, QtGui
+
+
+class SignalItem(QtGui.QStyledItemDelegate):
+#===========================================
+
+  def paint(self, painter, option, index):
+  #---------------------------------------
+    """
+    Paint a centered checkbox in the first column of the table
+    and draw a line underneath each table row,
+    """
+    r = option.rect
+    viewoption = QtGui.QStyleOptionViewItemV4(option)
+    if index.column() == 0:
+      margin = QtGui.QApplication.style().pixelMetric(QtGui.QStyle.PM_FocusFrameHMargin) + 1
+      viewoption.rect = QtGui.QStyle.alignedRect(option.direction, QtCore.Qt.AlignCenter,
+                                                 QtCore.QSize(option.decorationSize.width() + 5, option.decorationSize.height()),
+                                                 QtCore.QRect(r.x() + margin,
+                                                              r.y(),
+                                                              r.width() - (2 * margin),
+                                                              r.height()))
+    painter.drawLine(r.x(), r.y()+r.height(), r.x()+r.width(), r.y()+r.height())
+    QtGui.QStyledItemDelegate.paint(self, painter, viewoption, index)
+
+  def editorEvent(self, event, model, option, index):
+  #--------------------------------------------------
+    """
+    Toggle checkbox on mouse click and key press.
+    """
+    flags = model.flags(index)
+    if not (flags & (QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)):
+      return False
+    value = index.data(QtCore.Qt.CheckStateRole)
+    if not value.isValid(): return False
+    if not event.type() == QtCore.QEvent.MouseButtonRelease: return False
+    margin = QtGui.QApplication.style().pixelMetric(QtGui.QStyle.PM_FocusFrameHMargin) + 1
+    if not QtGui.QStyle.alignedRect(option.direction, QtCore.Qt.AlignCenter, option.decorationSize,
+                                    QtCore.QRect(option.rect.x() + (2 * margin),
+                                                 option.rect.y(),
+                                                 option.rect.width() - (2 * margin),
+                                                 option.rect.height())).contains(event.pos()): return False
+    state = QtCore.Qt.Unchecked if (value.toInt()[0] == QtCore.Qt.Checked) else QtCore.Qt.Checked
+    return model.setData(index, state, QtCore.Qt.CheckStateRole)
+
+
+class SignalTable(QtGui.QTableView):
+#====================================
+
+  def __init__(self, *args, **kwds):
+  #---------------------------------
+    QtGui.QTableView.__init__(self, *args, **kwds)
+    self.setItemDelegate(SignalItem(self))
+    self.setShowGrid(False)
+    self.verticalHeader().hide()
+    self.horizontalHeader().setStretchLastSection(True)
+    self.horizontalHeader().setHighlightSections(False)
+    self.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)
+    self.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
+    self._selectedrow = -1
+
+  def mousePressEvent(self, event):
+  #--------------------------------
+    row = self.rowAt(event.pos().y())
+    if row >= 0:
+      self.selectRow(row)
+      self._selectedrow = row
+    else:
+      self.clearSelection()
+    self.update()
+
+  def mouseMoveEvent(self, event):
+  #-------------------------------
+    row = self.rowAt(event.pos().y())
+    if self._selectedrow >= 0 and row >= 0 and self._selectedrow != row:
+      index = QtCore.QModelIndex()
+      self.model().moveRow(index, self._selectedrow,
+                           index, row if (row < self._selectedrow) else row + 1)
+      self._selectedrow = row
+      self.selectRow(row)
+    self.update()
+
+  def mouseReleaseEvent(self, event):
+  #----------------------------------
+    self._selectedrow = -1
+    QtGui.QTableView.mouseReleaseEvent(self, event)
