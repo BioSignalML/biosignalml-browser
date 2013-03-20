ac3cd468a8ba62507807c63e64fd2a5a7900c06b
Dave Brooks <dave@bcs.co.nz>
2013-03-20 18:56:22 +1300

    Add setup script for py2app and ignore build directories.

---
+++ chart/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+import shutil
+shutil.rmtree("build", ignore_errors=True)
+shutil.rmtree("dist", ignore_errors=True)
+
+APPNAME = 'QtBrowser'
+APP = ['QtBrowser.py']
+VERSION = '0.4.1'
+
+DATA_FILES = []
+## DATA_FILES = ['gpl-3.0.txt']
+
+OPTIONS = {'argv_emulation': False,
+           'compressed': False,  ## Otherwise pint can't get at its data file
+           'includes': ['PyQt4'],
+           'iconfile': 'QtBrowser.icns',
+           'excludes': ['modulegraph', 'graph_tool', 'sympy', 'scipy',
+                        'wx', 'matplotlib', 'OpenGL', 'zmq'],
+           'dylib_excludes': ['Tcl.framework','Tk.framework'],
+          }
+
+setup(
+app=APP,
+data_files=DATA_FILES,
+options={'py2app': OPTIONS},
+setup_requires=['py2app'],
+)
+
